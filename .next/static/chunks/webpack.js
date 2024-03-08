73307e843fbc1ab1bb9f4dace2996e31a9ffebdd
Hoang Quoc Anh <102196398+akaisaiza@users.noreply.github.com>
2024-03-08 10:24:36 +0700

    update

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -150,7 +150,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "753fc4ff3ded9b67"; }
+/******/ 		__webpack_require__.h = function() { return "cf863202398bd576"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- components/chat.js
+++ components/chat.js
@@ -11,6 +11,7 @@ const ChatComponent = () => {
     const apiService = new APIService();
     const [openModal, setOpenModal] = useState(false);
     const [selectedLocation, setSelectedLocation] = useState(null);
+    const [isTyping, setIsTyping] = useState(false);
 
     useEffect(() => {
         const el = document.getElementById('messages');
@@ -25,18 +26,20 @@ const ChatComponent = () => {
             setJwt(storedJwt);
         }
     }, [router]);
-    const onCloseModal = () => {
-        setOpenModal(false);
-    };
+
+    const onCloseModal = () => setOpenModal(false);
+
     const handleLocationClick = (id) => {
         setOpenModal(true);
         setSelectedLocation(id);
     };
+
     const sendMessage = async (e, message) => {
         e.preventDefault();
         if (!message.trim()) return;
 
         setNewMessage('');
+        setIsTyping(true); // Bắt đầu hiển thị typing effect
 
         const userMessage = {
             id: new Date().getTime(),
@@ -53,22 +56,29 @@ const ChatComponent = () => {
                 longitude: 0,
                 latitude: 0,
             }, jwt);
+
             const { answer, places } = response.response;
+            const parsedPlaces = JSON.parse(places);
+            const locationDetails = answer.split(/(?=\d\. ")/);
+
             const botMessage = {
                 id: new Date().getTime() + 1,
-                text: answer,
+                text: locationDetails,
                 sender: 'bot',
-                places: JSON.parse(places),
+                places: parsedPlaces,
             };
 
             setMessages((prevMessages) => [...prevMessages, botMessage]);
+            setIsTyping(false); // Dừng hiển thị typing effect
         } catch (error) {
             console.error('Error sending message:', error);
+            setIsTyping(false); // Dừng hiển thị typing effect nếu có lỗi
         }
     };
 
 
     const botAvatar = '/assets/goda_logo.png';
+
     return (
         <div className="flex-1 p-4 sm:p-8 justify-between flex flex-col custom-height-sm bg-gray-100 rounded-lg shadow">
             <div id="messages" className="flex flex-col space-y-4 p-4 overflow-y-auto scrollbar-thumb-blue scrollbar-thumb-rounded scrollbar-track-blue-lighter scrollbar-w-2 scrolling-touch">
@@ -78,46 +88,47 @@ const ChatComponent = () => {
                             <img src={botAvatar} alt="Bot Avatar" className="w-8 h-8 rounded-full mr-2" />
                         )}
                         <div className={`flex flex-col space-y-2 text-xs max-w-xs mx-2 items-${message.sender === 'user' ? 'end' : 'start'}`}>
-                            <div>
-                                <span className={`px-1 py-2 rounded-lg inline-block ${message.sender === 'user' ? 'bg-blue-500 text-white rounded-br-none' : 'bg-gray-300 text-gray-600 rounded-bl-none'}`}>
-                                    {message.text}
-                                </span>
-                            </div>
-                            {message.sender === 'bot' && (
-                                <div className="card gap-4 md:grid-cols-2 lg:grid-cols-4">
-                                    {message.places.map((place) => (
-                                        <div key={place.id} className="place-card p-4 my-4 border rounded-lg shadow-md">
-                                            <h3 className="text-xl font-semibold mb-2">{place.name}</h3>
-
-                                            <p className={`"text-gray-600" ${place.avg_rate ? "" : "hidden"}`}>Đánh giá: {place.avg_rate} ⭐</p>
-                                            <img src={place.image_link} alt={place.name} className="mt-2 rounded-lg" />
-                                            <div className='flex py-6'>
-                                                <div className="flex items-center mr-4"> {/* Container cho "Vị Trí" */}
-                                                    <a className='flex items-center tracking-wide py-2 px-3 sm:px-8 border border-blue-500 text-blue-500 bg-white-500 outline-none rounded-l-full rounded-r-full capitalize hover:bg-blue-500 hover:text-white-500 transition-all hover:shadow-blue' href={`https://www.google.com/maps/search/?api=1&query=${encodeURIComponent(place?.name)}`} target="_blank" rel="noopener noreferrer">
-                                                        <svg width="20px" height="20px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                                            <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
-                                                            <g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g>
-                                                            <g id="SVGRepo_iconCarrier">
-                                                                <path d="M12 21C15.5 17.4 19 14.1764 19 10.2C19 6.22355 15.866 3 12 3C8.13401 3 5 6.22355 5 10.2C5 14.1764 8.5 17.4 12 21Z" stroke="#27B3E2" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path>
-                                                                <path d="M12 12C13.1046 12 14 11.1046 14 10C14 8.89543 13.1046 8 12 8C10.8954 8 10 8.89543 10 10C10 11.1046 10.8954 12 12 12Z" stroke="#27B3E2" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path>
-                                                            </g>
-                                                        </svg>
-                                                        <div className=''>Vị Trí</div>
-                                                    </a>
+                            {message.sender === 'bot' ? (
+                                <div>
+                                    {message.text.map((text, index) => (
+                                        <div key={index}>
+                                            <span className={`px-1 py-2 rounded-lg inline-block bg-gray-300 text-gray-600 rounded-bl-none`}>
+                                                {text}
+                                            </span>
+                                            {message.places[index] && (
+                                                <div className="place-card p-4 my-4 border rounded-lg shadow-md">
+                                                    <h3 className="text-xl font-semibold mb-2">{message.places[index].name}</h3>
+                                                    <p className={`"text-gray-600" ${message.places[index].avg_rate ? "" : "hidden"}`}>Đánh giá: {message.places[index].avg_rate} ⭐</p>
+                                                    <img src={message.places[index].image_link} alt={message.places[index].name} className="mt-2 rounded-lg" />
+                                                    <div className='flex py-6'>
+                                                        <button className='flex items-center tracking-wide py-2 px-3 sm:px-8 border border-blue-500 text-blue-500 bg-white-500 outline-none rounded-l-full rounded-r-full capitalize hover:bg-blue-500 hover:text-white-500 transition-all hover:shadow-blue' onClick={() => handleLocationClick(message.places[index].id)}>Chi tiết</button>
+                                                    </div>
                                                 </div>
-                                                <button className='flex items-center tracking-wide py-2 px-3 sm:px-8 border border-blue-500 text-blue-500 bg-white-500 outline-none rounded-l-full rounded-r-full capitalize hover:bg-blue-500 hover:text-white-500 transition-all hover:shadow-blue' onClick={() => handleLocationClick(place.id)}>Chi tiết</button>
-                                            </div>
+                                            )}
                                         </div>
                                     ))}
                                 </div>
+                            ) : (
+                                <span className={`px-1 py-2 rounded-lg inline-block bg-blue-500 text-white rounded-br-none`}>
+                                    {message.text}
+                                </span>
                             )}
                         </div>
-
                         {message.sender === 'user' && (
-                            <img src={'https://cdn.icon-icons.com/icons2/1378/PNG/512/avatardefault_92824.png'} alt="Bot Avatar" className="w-8 h-8 rounded-full mr-2" />
+                            <img src={'https://cdn.icon-icons.com/icons2/1378/PNG/512/avatardefault_92824.png'} alt="User Avatar" className="w-8 h-8 rounded-full mr-2" />
                         )}
                     </div>
                 ))}
+                {isTyping && (
+                    <div className="flex justify-start">
+                        <img src={botAvatar} alt="Bot Avatar" className="w-8 h-8 rounded-full mr-2" />
+                        <div className="typing-indicator">
+                            <span></span>
+                            <span></span>
+                            <span></span>
+                        </div>
+                    </div>
+                )}
             </div>
             <div className="px-4 pt-4 mb-2 sm:mb-0 bg-white">
                 <form className="relative flex" onSubmit={(e) => sendMessage(e, newMessage)}>
